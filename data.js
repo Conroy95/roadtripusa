c5d1e8912b3581b0e150e5ab3ec8d2eea319389d
Conroy95 <leandervdneut@ziggo.nl>
2025-01-14 23:44:37 +0100

    Update and rename data.json to data.js

--- data.js
+++ data.js
@@ -1,6 +1,27 @@
-[
-    {
-        "day": 1,
+// data.js
+const roadtripData = [
+  {
+    day: 1,
+    from: "Los Angeles, CA",
+    to: "Las Vegas, NV",
+    stops: [
+      { name: "Calico Ghost Town", coords: "34.9428, -116.8653", img: "images/calico.jpg" },
+      { name: "Mojave National Preserve", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
+    ],
+    hotel: { name: "Bellagio Hotel", coords: "36.1126, -115.1767", img: "images/bellagio.jpg" }
+  },
+  {
+    day: 2,
+    from: "Las Vegas, NV",
+    to: "Grand Canyon, AZ",
+    stops: [
+      { name: "Hoover Dam", coords: "36.0156, -114.7378", img: "images/hoover.jpg" },
+      { name: "Route 66 Museum", coords: "35.1895, -114.0530", img: "images/route66.jpg" },
+    ],
+    hotel: { name: "Yavapai Lodge", coords: "36.0521, -112.1230", img: "images/yavapai.jpg" }
+  },
+  // Voeg 20 extra dagen toe met dezelfde structuur...
+];
         "from": "New York JFK Airport",
         "to": "LAX Airport",
         "hotel": {
