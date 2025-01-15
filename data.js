0efd4c292a42a0e06aaa208c0843b18dfe1d38c1
Conroy95 <leandervdneut@ziggo.nl>
2025-01-15 08:39:47 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,20 +5,21 @@ const roadtripData = [
     from: "New York JFK Airport",
     to: "LAX Airport",
     stops: [
-      { name: "Calico Ghost Town", coords: "34.9428, -116.8653", img: "images/calico.jpg" },
-      { name: "Mojave National Preserve", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
+      { name: "Santa Barbara", coords: "34.9428, -116.8653", img: "images/calico.jpg" },
+      { name: "Vandenberg Space Force Base Visitor Cente", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
+      { name: "Pismo Beach", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
     ],
     hotel: { name: "Hampton Inn By Hilton Los Angeles Airport", coords: "33.942722, -118.369611", img: "https://conroy95.github.io/RoadTrip95/images/hotel1.jpg" }
   },
   {
     day: 2,
-    from: "Las Vegas, NV",
-    to: "Grand Canyon, AZ",
+    from: "Los Angeles",
+    to: "Cambria",
     stops: [
       { name: "Hoover Dam", coords: "36.0156, -114.7378", img: "images/hoover.jpg" },
       { name: "Route 66 Museum", coords: "35.1895, -114.0530", img: "images/route66.jpg" },
     ],
-    hotel: { name: "Yavapai Lodge", coords: "36.0521, -112.1230", img: "images/yavapai.jpg" }
+    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "https://conroy95.github.io/RoadTrip95/images/hotel2.jpg" }
   },
   // Voeg 20 extra dagen toe met dezelfde structuur...
 ];
