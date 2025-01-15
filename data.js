6ca8b53697880f1cc9c905d979de425b2a071edc
Conroy95 <leandervdneut@ziggo.nl>
2025-01-15 08:41:28 +0100

    Update data.js

--- data.js
+++ data.js
@@ -22,5 +22,16 @@ const roadtripData = [
     ],
     hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "https://conroy95.github.io/RoadTrip95/images/hotel2.jpg" }
   },
+  {
+    day: 3,
+    from: "Los Angeles",
+    to: "Cambria",
+    stops: [
+      { name: "Santa Barbara", coords: "34.9428, -116.8653", img: "images/calico.jpg" },
+      { name: "Vandenberg Space Force Base Visitor Cente", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
+      { name: "Pismo Beach", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
+    ],
+    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "https://conroy95.github.io/RoadTrip95/images/hotel2.jpg" }
+  },
   // Voeg 20 extra dagen toe met dezelfde structuur...
 ];
