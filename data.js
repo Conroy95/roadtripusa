a09498ccf6e1ed9cfaac6a16f1e9f370499b5ca6
Conroy95 <leandervdneut@ziggo.nl>
2025-01-15 09:36:24 +0100

    Update data.js

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const roadtripData = [
       { name: "Vandenberg Space Force Base Visitor Cente", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
       { name: "Pismo Beach", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
     ],
-    hotel: { name: "Hampton Inn By Hilton Los Angeles Airport", coords: "33.942722, -118.369611", img: "https://conroy95.github.io/RoadTrip95/images/hotel1.jpg" }
+    hotel: { name: "Hampton Inn By Hilton Los Angeles Airport", coords: "33.942722, -118.369611", img: "images/hotel1.png" }
   },
   {
     day: 2,
@@ -20,7 +20,7 @@ const roadtripData = [
       { name: "Vandenberg Space Force Base Visitor Cente", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
       { name: "Pismo Beach", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
     ],
-    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "https://conroy95.github.io/RoadTrip95/images/hotel2.jpg" }
+    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "images/hotel2.png" }
   },
   {
     day: 3,
@@ -31,7 +31,7 @@ const roadtripData = [
       { name: "Vandenberg Space Force Base Visitor Cente", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
       { name: "Pismo Beach", coords: "35.0121, -115.4708", img: "images/mojave.jpg" },
     ],
-    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "https://conroy95.github.io/RoadTrip95/images/hotel2.jpg" }
+    hotel: { name: "Cambria Pines Lodge", coords: "35.558722, -121.080111", img: "images/hotel3.png" }
   },
   // Voeg 20 extra dagen toe met dezelfde structuur...
 ];
